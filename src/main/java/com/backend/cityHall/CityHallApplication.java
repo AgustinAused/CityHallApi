a259b0ab3d10359624c81a4a41a9b3a847ccfafd
gashino <agaggino@uade.edu.ar>
2024-04-23 16:26:50 -0300

    test push

--- src/main/java/com/backend/cityHall/CityHallApplication.java
+++ src/main/java/com/backend/cityHall/CityHallApplication.java
@@ -3,6 +3,7 @@ package com.backend.cityHall;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//test jeje
 @SpringBootApplication
 public class CityHallApplication {
 
