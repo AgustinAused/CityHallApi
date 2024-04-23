45f9cd50c59dcb943d78bd079c5e448f28f1c063
Agustin Aused <agus.aused@gmail.com>
2024-04-23 16:46:22 -0300

    confiuracion de la base de datos
    Signed-off-by: Agustin Aused <agus.aused@gmail.com>

--- src/main/java/com/backend/cityHall/config/database/DataBaseConfig.java
+++ src/main/java/com/backend/cityHall/config/database/DataBaseConfig.java
@@ -1,2 +1,30 @@
-package com.backend.cityHall.config.database;public class DataBaseConfig {
+package com.backend.cityHall.config.database;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataBaseConfig {
+
+        @Bean
+        @ConfigurationProperties(prefix = "datasource.my-config")
+        public DataSource dataSource(){
+            return DataSourceBuilder.create().build();
+        }
+
+        @Bean
+        public JdbcTemplate jdbcTemplate(){
+            return new JdbcTemplate(dataSource());
+        }
+
+        @Bean
+        public NamedParameterJdbcTemplate namedParameterJdbcTemplate(){
+            return new NamedParameterJdbcTemplate(dataSource());
+        }
 }
